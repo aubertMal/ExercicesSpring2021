740a4e926f6d6d3d3a8a6a5ef466dc57a486bc6b
aubertMal <malek.aubert@gmail.com>
2021-04-26 11:52:36 +0200

    integration du repo Spring

---
+++ aubertma/src/main/java/org/aubm/aubertma/AubertmaApplication.java
@@ -0,0 +1,13 @@
+package org.aubm.aubertma;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AubertmaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AubertmaApplication.class, args);
+	}
+
+}
---
+++ aubertma/src/test/java/org/aubm/aubertma/AubertmaApplicationTests.java
@@ -0,0 +1,13 @@
+package org.aubm.aubertma;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AubertmaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
