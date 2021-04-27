3f171ff49cf054d0a61a1436d9be80d68bd3bdbc
aubertMal <malek.aubert@gmail.com>
2021-04-27 09:09:08 +0200

    update controller

--- aubertma/src/main/java/org/aubm/aubertma/controller/CustomerController.java
+++ aubertma/src/main/java/org/aubm/aubertma/controller/CustomerController.java
@@ -4,13 +4,11 @@ import org.aubm.aubertma.entity.Customer;
 import org.aubm.aubertma.service.CustomerService;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
+
 import java.util.List;
 
-@RequestController
+@RestController
 @RequestMapping("/customers")
 public class CustomerController {
 
@@ -30,8 +28,13 @@ public class CustomerController {
     }
     @PostMapping
     public void addCustomer(@RequestBody Customer newCustomer){
+        customerService.addCustomer(newCustomer);
         System.out.println("ajout d'un nouveau client" + newCustomer.getName());
     }
 
-
+    @DeleteMapping
+    public void deleteCustomer(int id){
+        customerService.removeCustomer(id);
+        System.out.println("suppression du client");
+    }
 }
