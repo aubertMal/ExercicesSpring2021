f37b7518737ed545622b59290499ae020303563a
aubertMal <malek.aubert@gmail.com>
2021-04-29 11:26:24 +0200

    add front with thymeleaf

--- aubertma/src/main/java/org/aubm/aubertma/controller/ViewController.java
+++ aubertma/src/main/java/org/aubm/aubertma/controller/ViewController.java
@@ -1,13 +1,11 @@
 package org.aubm.aubertma.controller;
-
 import org.aubm.aubertma.entity.Customer;
 import org.aubm.aubertma.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
 
-import java.util.List;
 
 @Controller
 @RequestMapping
@@ -15,10 +13,29 @@ public class ViewController {
     @Autowired
     CustomerService customerService;
 
-    @GetMapping("/view")
-    public List<Customer> getCustomers(){
-        System.out.println("got the request!!!!");
-        return customerService.getCustomers();
+    @GetMapping("/view-by-id/{id}")
+    public String viewById(@PathVariable String id, Model model){
+        model.addAttribute("customer",customerService.getCustomerById(Integer.parseInt(id)));
+        return "view-by-id";
     }
 
+    @GetMapping("/view")
+    public String view(Model model){
+        model.addAttribute("customers",customerService.getCustomers());
+        return "view";
+    }
+
+    @GetMapping("/showAddCustomer")
+    public String showAddCustomer(Model model){
+        Customer newCustomer = new Customer(0,"","");
+        model.addAttribute("Customer",newCustomer);
+
+        return "addCustomer";
+    }
+
+    @PostMapping("/addCustomer")
+    public String addCustomer(Model model, @ModelAttribute("Customer") Customer newCustomer){
+        customerService.addCustomer(new Customer(newCustomer.getId(), newCustomer.getName(), newCustomer.getCodeINSEE()));
+        return "redirect:/view";
+    }
 }
